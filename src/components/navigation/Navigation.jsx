c3b2511bb02e0fbe509d68081c1bfdae5485e7ed
NAZARIILUNYCH <47526149+NAZARIILUNYCH@users.noreply.github.com>
2021-02-12 11:25:47 +0200

    FE-15

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,17 +3,16 @@ import React from 'react';
 import { days } from '../../utils/dateUtils.js';
 
 const Navigation = ({ weekDates }) => {
-
-    return (
-        <header className="calendar__header">
-            {weekDates.map(dayDate =>
-                <div className="calendar__day-label day-label">
-                    <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-                    <span className="day-label__day-number">{dayDate.getDate()}</span>
-                </div>
-            )}
-        </header>
-    )
-}
+  return (
+    <header className="calendar__header">
+      {weekDates.map(dayDate => (
+        <div className="calendar__day-label day-label">
+          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
+          <span className="day-label__day-number">{dayDate.getDate()}</span>
+        </div>
+      ))}
+    </header>
+  );
+};
 
 export default Navigation;
